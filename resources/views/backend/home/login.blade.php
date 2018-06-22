d9edb1f17bec28630d629348c26cf35a705fef23
haule <hauln@leverages.jp>
2018-06-22 10:51:11 +0700

    add login page

---
+++ resources/views/backend/home/login.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <link href="/css/backend/bootstrap.min.css" media="all" rel="stylesheet" type="text/css" />
+        <link href="/css/backend/sb-admin.css" media="all" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="card card-login mx-auto mt-5">
+                <div class="card-header">ログインパネル</div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <input class="form-control" id="username" type="email" aria-describedby="usernameHelp" placeholder="ユーザ名">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" id="password" type="password" placeholder="パースワード">
+                        </div>
+                        <button class="btn btn-primary btn-block">ログイン</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
