cfe343d7f4e51b0672b79eaebd1f0a274c7ea4e6
haule <hauln@leverages.jp>
2018-06-23 11:15:04 +0700

    add test page

---
+++ resources/views/backend/home/detail.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.backend.master')
+@section('title', 'Detail')
+@section('content')
+    <div class="card">
+        <div class="card-header">My Dashboard</div>
+        <div class="card-body">
+            <form action="/" id="MessageAdminIndexForm" enctype="multipart/form-data" method="post" accept-charset="utf-8">
+                <table class="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td class="aside">TEXT INPUT 1</td>
+                            <td>
+                                <input name="" class="form-control" id=""></input>
+                            </td>
+                            <td class="desc"></td>
+                        </tr>
+                        <tr>
+                            <td class="aside">TEXT INPUT 2</td>
+                            <td>
+                                <input name="" class="form-control" id=""></input>
+                            </td>
+                            <td class="desc"></td>
+                        </tr>
+                        <tr>
+                            <td class="aside">TEXT</td>
+                            <td>
+                                <textarea name="" rows="5" class="form-control" cols="30" id=""></textarea>
+                            </td>
+                            <td class="desc"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="form-group text-center">
+                    <input type="submit" class="btn btn-primary btn-sm" value="更新">
+                </div>
+            </form>
+        </div>
+    </div>
+@stop
