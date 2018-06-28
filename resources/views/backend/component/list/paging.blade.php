675d9a24da33c4221be214f2f443b67338cfead1
Hai TM <haitm@leverages.jp>
2018-06-28 10:27:00 +0700

    paging component

---
+++ resources/views/backend/component/list/paging.blade.php
@@ -0,0 +1,73 @@
+<div class="card">
+    <div class="card-header">
+        <i class="fa fa-list"></i>
+        {{ $table['title'] }}
+    </div>
+    <div class="card-body">
+        @yield('list_header')
+        <div class="table-responsive">
+            <div id="dataTable_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+                <div class="row">
+                    <table width="100%" class="table table-striped table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                @foreach ($table['header'] as $item)
+                                    <th>{{ $item }}</th>
+                                @endforeach
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $item)
+                            <tr>
+                                @foreach ($table['body'] as $row)
+                                <td
+                                    @if (!empty($row['attr']))
+                                        @foreach($row['attr'] as $key => $value)
+                                            {{ $key }}="{{ $value }}"
+                                        @endforeach
+                                    @endif
+                                >
+                                    @if ($row['field'])
+                                        {{ $item[$row['field']] }}
+                                    @elseif ($row['tpl'])
+                                        @if (empty($row['tpl_arg']))
+                                            {{ $row['tpl'] }}
+                                        @else
+                                            @foreach($row['tpl_arg'] as $key => $value)
+                                                {!! str_replace($key, $item[$value], $row['tpl']) !!}
+                                            @endforeach
+                                        @endif
+                                    @endif
+                                </td>
+                                @endforeach
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- pagination -->
+                <div class="row">
+                    @php
+                        $pager_total_item = $data->total();
+                        $pager_begin = ($data->currentpage () - 1) * ($data->perpage() + 1);
+                        $pager_end = ($data->currentpage ()) * ($data->perpage());
+                        $pager_current_page = $data->currentPage();
+                        $pager_total_page = $data->count();
+                    @endphp
+                    <div class="col-sm-12 col-md-5">
+                        <div class="dataTables_info" id="dataTable_info" role="status" aria-live="polite"><!-- Showing 1 to 10 of 57 entries -->
+                            {{ $pager_total_item }}件
+                            {{ $pager_begin }}~{{ $pager_end }}を表示
+                            {{ $pager_current_page }}/{{ $pager_total_page}}ページ
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-12">
+                        <div class="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
+                             {!! $data->links() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
