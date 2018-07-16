13feb513fb21dc0c3f2aa32daa3ec72bef49b973
haule <hauln@leverages.jp>
2018-07-16 13:01:05 +0700

    modify login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,6 +36,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('web')->except('logout');
+    }
+
+    protected function credentials(Request $request) {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+            'role' => USER_ROLE_PUBLIC,
+            'mode' => USER_MODE_ENABLE,
+            'deleted_at' => 'NULL'
+        ];
+        return array_merge($credentials);
     }
 
     public function getLogout()
