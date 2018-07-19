14d2f93521754e85c9c12f1a892f0adafe2515d7
Hai TM <haitm@leverages.jp>
2018-07-19 13:48:13 +0700

    Affiliator

--- app/Models/Backend/Affiliator/Affiliator.php
+++ app/Models/Backend/Affiliator/Affiliator.php
@@ -3,9 +3,12 @@
 namespace App\Models\Backend\Affiliator;
 
 use App\Models\Backend\Base;
+use App\Models\Backend\Traits\Affiliator\AffiliatorUser;
 
 class Affiliator extends Base
 {
+    use AffiliatorUser;
+
     public $fillable = [
         'fullname',
         'email',
