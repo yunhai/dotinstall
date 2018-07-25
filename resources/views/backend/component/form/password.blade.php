4cf39c26561ecc778d24dbac271f563386e39825
haule <hauln@leverages.jp>
2018-07-25 10:13:07 +0700

    password

---
+++ resources/views/backend/component/form/password.blade.php
@@ -0,0 +1,16 @@
+<tr>
+    <td class="aside">{{ $field_label }}</td>
+    <td>
+        <input name="{{ $field_name }}" class="form-control" type="{{ $field_type }}"
+            @if (!empty($field_attribute))
+            @foreach ($field_attribute as $key => $value)
+                {{ $key }}="{{ $value }}"
+            @endforeach
+            data-name="{{ $field_name }}"
+            @endif
+        ></input>
+        @if (!empty($errors->first($field_name)))
+            <span class="text-danger d-block mt-2">{{ $errors->first($field_name) }}</span>
+        @endif
+    </td>
+</tr>
