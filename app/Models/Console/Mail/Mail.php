3fb6c4ff6d2a5c0f8c6ca528d463366a35ed5107
Hai TM <haitm@leverages.jp>
2018-07-26 11:22:18 +0700

    update

--- app/Models/Console/Mail/Mail.php
+++ app/Models/Console/Mail/Mail.php
@@ -3,6 +3,7 @@
 namespace App\Models\Console\Mail;
 
 use App\Models\Console\Base;
+use Carbon\Carbon;
 
 class Mail extends Base
 {
@@ -28,8 +29,12 @@ class Mail extends Base
 
     public function updadeSentModeByIdList(array $list = [])
     {
+        $update = [
+            'flush_date' => Carbon::now()->format('Y-m-d H:i:s'),
+            'mode' => MAIL_MODE_SENT,
+        ];
         $this->whereIn('id', $list)
-            ->update(['mode' => MAIL_MODE_SENT]);
+            ->update($update);
         return true;
     }
 }
