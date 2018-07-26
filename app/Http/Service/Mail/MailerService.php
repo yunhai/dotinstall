6ffb533c8cffa6b6ae9c306693f829ea336a83d5
Hai TM <haitm@leverages.jp>
2018-07-26 13:00:51 +0700

    .

--- app/Http/Service/Mail/MailerService.php
+++ app/Http/Service/Mail/MailerService.php
@@ -13,11 +13,10 @@ class MailerService
             $mailable = new $name($mail);
         }
 
-        $mailer = Mail::subject($mail['title']);
-        $mailer = call_user_func_array(['mailer', 'to'], $mail['to']);
+        $mailer = call_user_func_array(['Mail', 'to'], $mail['to']);
         if ($mail['bcc']) {
             foreach ($mail['bcc'] as $bcc) {
-                $mailer = call_user_func_array(['mailer', 'bcc'], $bcc);
+                $mailer = call_user_func_array(['Mail', 'bcc'], $bcc);
             }
         }
 
