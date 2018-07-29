50ab66f829385881c12457d480c1978e933a9b3f
pandog <yunhaihuang@gmail.com>
2018-07-29 16:13:30 +0700

    update

--- resources/assets/jsx/backend/component/upload/chunk_upload.js
+++ resources/assets/jsx/backend/component/upload/chunk_upload.js
@@ -76,7 +76,7 @@ export default class ChuckUpload {
             </video>
 
             <div class='dd-control'>
-                <a href='/backend/media/download/${obj.id}' class='btn btn-outline-info btn-sm' title='ダウンロード'>ダウンロード</a>
+                <a href='/admin/media/download/${obj.id}' class='btn btn-outline-info btn-sm' title='ダウンロード'>ダウンロード</a>
                 <span class='j-dd-remove btn btn-outline-danger btn-sm' title='削除'>削除</span>
             </div>
         `;
@@ -121,7 +121,7 @@ export default class ChuckUpload {
             <input type='hidden' value='${obj.original_name}' name='${name}[${obj.id}][original_name]'/>
             <img width="400" src="${obj.url}" class='dd-preview-image'/>
             <div class='dd-control'>
-                <a href='/backend/media/download/${obj.id}' class='btn btn-outline-info btn-sm' title='ダウンロード'>ダウンロード</a>
+                <a href='/admin/media/download/${obj.id}' class='btn btn-outline-info btn-sm' title='ダウンロード'>ダウンロード</a>
                 <span class='j-dd-remove btn btn-outline-danger btn-sm' title='削除'>削除</span>
             </div>
         `;
@@ -140,7 +140,7 @@ export default class ChuckUpload {
             <div class='j-filename_holder filename_holder'></div>
             <div class='j-language_holder language_holder'></div>
             <div class='dd-control'>
-                <a href='/backend/media/download/${obj.id}' class='btn btn-outline-info btn-sm' title='ダウンロード'>ダウンロード</a>
+                <a href='/admin/media/download/${obj.id}' class='btn btn-outline-info btn-sm' title='ダウンロード'>ダウンロード</a>
                 <span class='j-dd-remove btn btn-outline-danger btn-sm' title='削除'>削除</span>
             </div>
         `;
