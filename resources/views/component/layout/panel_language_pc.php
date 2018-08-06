4eb63c9d41f66b0c953c69791261d1ea4b7546d8
pandog <yunhaihuang@gmail.com>
2018-08-06 22:53:47 +0900

    fix bug

---
+++ resources/views/component/layout/panel_language_pc.php
@@ -0,0 +1,33 @@
+<div class="box box-panel box-panel-language mb-0">
+	<div class="card">
+		<div class="card-img-overlay px-5">
+			<div class="card-body">
+				<div class="row row align-items-center">
+					<div class="card-body-language-left">
+						<div class="card-language card-language-btn text-center">
+							<span class="mar_r10">
+								<img class="card-img card-img-app" src="/img/img_app.png">
+							</span>
+							<span class="mar_r10">
+								<img class="card-img card-img-objective_c" src="/img/img_objective_c.png">
+							</span>
+							<span>
+								<img class="card-img card-img-swift" src="/img/img_swift.png">
+							</span>
+						</div>
+						<div class="card-language card-language-text">
+							<p class="card-text text-white">IPhoneアプリ！　swift、Objective-C</p>
+							<p class="card-text text-white">動画数は、今からどんどん増えます！</p>
+							<p class="card-text text-white">動画増えても料金は変わらず、見放題！</p>
+						</div>	
+					</div>
+					<div class="card-body-language-right">
+						<span class="card-right-img-980">
+							<img class="card-img card-img-980" src="/img/img_980.png">
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
