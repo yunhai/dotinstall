2762fda7befff33c0f7188a9fa2997cb8b34dff5
Hai TM <haitm@leverages.jp>
2018-08-07 09:34:55 +0700

    email

--- resources/views/emails/user/activation.blade.php
+++ resources/views/emails/user/activation.blade.php
@@ -1,3 +1,6 @@
-下記のURLにアクセスしてください。<br>
-
-<a href="{{ $user->activation_link }}">{{ $user->activation_link }}</a>
+新規ご登録ありがとうございます。<br>
+<br>
+下記のＵＲＬにアクセスしてメールアドレス確認をお願いします。<br>
+<a href="{{ $user->activation_link }}">{{ $user->activation_link }}</a><br>
+<br>
+プログラミングＧＯ！<br>
