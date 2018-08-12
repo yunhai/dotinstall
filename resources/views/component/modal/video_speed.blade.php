d8df2e7c693ea30e102c3addee55142ccb37097c
pandog <yunhaihuang@gmail.com>
2018-08-12 21:54:17 +0900

    fix bug

--- resources/views/component/modal/video_speed.blade.php
+++ resources/views/component/modal/video_speed.blade.php
@@ -5,8 +5,8 @@
                 <p class="mb-0">無料動画は、速度調整できません。<br/>月額会員になるとご利用できます。</p>
             </div>
             <div class="modal-footer" style="padding: 5px;text-align: center; display: block; width: 100%">
-                <a href="javascript:;" class="btn btn-sm btn-secondary"  data-dismiss="modal" aria-label="Close" style="padding-bottom: 2px;">後で月額会員になる</a>
-                <a href="@logined {{ route('user.upgrade') }} @else  {{ route('register.diamond') }} @endlogined"class="btn btn-sm btn-primary" style="padding-bottom: 2px;">月額会員になる</a>
+                <a href="javascript:;" class="btn btn-sm btn-secondary mr-0"  data-dismiss="modal" aria-label="Close" style="padding-bottom: 2px;">後で月額会員になる</a>
+                <a href="@logined {{ route('user.upgrade') }} @else  {{ route('register.diamond') }} @endlogined"class="btn btn-sm btn-primary" style="padding-bottom: 2px; width: 126px;">月額会員になる</a>
             </div>
         </div>
     </div>
