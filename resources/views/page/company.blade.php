2ef5ec77083cffe0a752626566f59e73450cefba
pandog <yunhaihuang@gmail.com>
2018-08-13 18:25:59 +0900

    Quang : update company text

--- resources/views/page/company.blade.php
+++ resources/views/page/company.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="card-body">
                     <div class="form-group">
-                        <p>【社名】株式会社プログラミングゴー<p>
+                        <p>【社名】プログラミングゴー<p>
                     </div>
                     <div class="form-group">
                         <p>【代表取締役社長】 　田中　誠<p>
@@ -20,10 +20,13 @@
                         <p>【所在地】	東京都港区三田2丁目4-3</p>
                     </div>
                     <div class="form-group">
+                        <p>【電話番号】(050) 5532-8183</p>
+                    </div>
+                    <div class="form-group">
                         <p>【設立】2015年3月18日</p>
                     </div>
                     <div class="form-group mb-0">
-                        <p class="card-text">【事業内容】初心者向けプログラミング勉強動画、実践型で楽しく学んでいく５分動画事業</p>
+                        <p class="card-text">【事業内容】初心者向けプログラミング勉強動画、実践型で楽しく学んでいく５分動画レッスン事業</p>
                     </div>
                 </div>
             </div>
