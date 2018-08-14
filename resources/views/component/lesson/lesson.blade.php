25bda1e6dd094e2bd251407838bba4f12553b46b
pandog <yunhaihuang@gmail.com>
2018-08-14 21:49:37 +0900

    fix bug

--- resources/views/component/lesson/lesson.blade.php
+++ resources/views/component/lesson/lesson.blade.php
@@ -5,11 +5,25 @@
             $cate = array_get($filter_form, 'input_value.category');
             $keyword = array_get($filter_form, 'input_value.keyword');
         @endphp
+        @pc
+        	@php
+        		$class_form_group = "col-3";
+        		$class_form_label = "col-3";
+        		$class_select = "col-7";
+        	@endphp
+        @endpc
+        @sp
+        	@php
+        		$class_form_group = "col-12";
+        		$class_form_label = "col-2";
+        		$class_select = "col-10";
+        	@endphp
+        @endsp
         <div class="form-group form-group-search-lesson row justify-content-center">
-            <div class="@pc col-3 @endpc @sp col-12 @endsp">
+            <div class="{{ $class_form_group }}">
                 <div class="input-group input-group-difficulty">
-                    <label for="level" class="col-3 col-form-label px-0">段階</label>
-                    <select name="difficulty" class="@pc col-7 @endpc @sp col-9 @endsp">
+                    <label for="level" class="{{ $class_form_label }} col-form-label px-0">段階</label>
+                    <select name="difficulty" class="{{ $class_select }}">
                         <option value="" @if (empty($diff)) selected @endif>全ての動画</option>
                         @foreach ($filter_form['difficulty'] as $difficulty_id => $difficulty)
                             <option value="{{ $difficulty_id }}" @if ($diff == $difficulty_id) selected @endif>{{ $difficulty }}</option>
@@ -17,10 +31,10 @@
                     </select>
                 </div>
             </div>
-            <div class="@pc col-3 @endpc @sp col-12 @endsp">
+            <div class="{{ $class_form_group }}">
                 <div class="input-group input-group-category">
-                    <label for="level" class="col-3 col-form-label px-0">カテゴリ</label>
-                    <select name="category" class="@pc col-7 @endpc @sp col-9 @endsp">
+                    <label for="level" class="{{ $class_form_label }} col-form-label px-0">カテゴリ</label>
+                    <select name="category" class="{{ $class_select }}">
                         <option value="" @if (empty($cate)) selected @endif>全ての動画</option>
                         @foreach ($filter_form['category'] as $cat_id => $cat)
                             <option value="{{ $cat_id }}" @if ($cate == $cat_id) selected @endif>{{ $cat }}</option>
@@ -28,7 +42,7 @@
                     </select>
                 </div>
             </div>
-            <div class="@pc col-3 @endpc @sp col-12 @endsp">
+            <div class="{{ $class_form_group }}">
                 <div class="input-group">
                     <input type="text" class="form-control form-control-search" name="keyword" value="@if (!empty($keyword)) {{ $keyword }} @endif" placeholder="動画検索">
                     <button class="btn-search">検索</button>
