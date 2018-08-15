5e0c2017b78c8e1b2d4df778fe93e51d078a402e
pandog <yunhaihuang@gmail.com>
2018-08-15 21:14:16 +0900

    fix bug

--- resources/views/component/layout/panel_youtube_pc.blade.php
+++ resources/views/component/layout/panel_youtube_pc.blade.php
@@ -3,7 +3,7 @@
         <img class="card-img card-img-panel" src="/img/panel.jpg">
         <div class="card-img-overlay px-5">
             <div class="card-body" @pc style="padding-top: .5rem; padding-left: 70px; padding-right: 70px;" @endpc>
-                <h5 class="card-text card-text-header text-center font-weight-bold mb-0">小学１年生からプログラミング動画！</h5>
+                <h5 class="card-text card-text-header text-center font-weight-bold mb-0">小学生からプログラミング動画！</h5>
                 <div class="row @pc align-items-end @endpc">
                     <div class="pc-card-sign card-sign p-0" id="pc-panel-left">
                         <p class="card-text card-text-sign">５分動画！小学生から大人まで！</p>
--- resources/views/component/layout/panel_youtube_sp.blade.php
+++ resources/views/component/layout/panel_youtube_sp.blade.php
@@ -2,7 +2,7 @@
     <div class="card">        
         <div class="card-img-overlay px-5">
             <div class="card-body card-body-text">
-                <h5 class="card-text card-text-header text-center font-weight-bold mb-0">小学１年生からプログラミング動画！</h5>
+                <h5 class="card-text card-text-header text-center font-weight-bold mb-0">小学生からプログラミング動画！</h5>
                 <div class="row">
                     <div class="card-sign p-0" id="pc-panel-left">
                         <p class="card-text card-text-sign">５分動画！小学生から大人まで！</p>
