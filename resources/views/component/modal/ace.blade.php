d5db230b058e5837aca7e9c7175189e0777be467
pandog <yunhaihuang@gmail.com>
2018-08-18 18:47:19 +0900

    fix bug

--- resources/views/component/modal/ace.blade.php
+++ resources/views/component/modal/ace.blade.php
@@ -11,7 +11,7 @@
             <div class="modal-body">
 	            @if (!empty($resources_item))
 					<p>素材ダウンロード
-						<a href="{{ route('lesson_detail.resource.download', compact('lesson_id', 'lesson_detail_id')) }}" class="btn btn-sm btn-download ml-5 btn--resource" role="button" aria-pressed="true">素材を一括ダウンロードする</a>
+						<a href="{{ route('lesson_detail.resource.download', compact('lesson_id', 'lesson_detail_id')) }}" class="btn btn-sm btn-download @pc ml-5 @endpc btn--resource" role="button" aria-pressed="true">素材を一括ダウンロードする</a>
 					</p>
 		            <div class="row" style="margin-bottom: 1rem;">
 			            @foreach($resources_item as $res)
@@ -19,9 +19,9 @@
 			            		$media_id = $res['id']; 
 			            		$path = $res['path'];
 			            	@endphp
-			            	<div class="col-2">
+			            	<div class="@pc col-2 @endpc @sp col-4 @endsp">
 				            	<div class="card">
-					            	<img class="img-thumbnail" src="@media_path($path)" style="width: 200px; height: 78px;">
+					            	<img class="img-thumbnail" src="@media_path($path)" style="height: 78px;">
 					            	<div class="card-body p-1">
 										<p class="card-text text-center"><a href="{{ route('media.download', $media_id) }}">ダウンロード</a></p>
 					            	</div>
