6637ea2221311786951e0d97a5d87cfa386acf00
pandog <yunhaihuang@gmail.com>
2018-08-19 09:16:34 +0900

    fix bug

--- resources/views/component/lesson/lesson_detail/lesson_detail_info_sp.blade.php
+++ resources/views/component/lesson/lesson_detail/lesson_detail_info_sp.blade.php
@@ -29,7 +29,8 @@
 		@else
 			<div class="row-resource">
 				<div class="col">
-					<a href="javascript:;" class="btn btn-download-resource">素材を一括ダウンロードする</a>（有料会員になるとダウンロードできます。）
+					<a href="javascript:;" class="btn btn-download-resource">素材を一括ダウンロードする</a>
+					<p class="card-text">（有料会員になるとダウンロードできます。）</p>
 				</div>
 			</div>
 		@endif
