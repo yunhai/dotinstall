2062a8ee7fb8eeae49b7474a54e0c73f4dc98bdd
pandog <yunhaihuang@gmail.com>
2018-08-21 23:09:52 +0700

    check agreement

--- resources/assets/js/payment.js
+++ resources/assets/js/payment.js
@@ -84,16 +84,19 @@ function stripeTokenHandler(token) {
 $('#j-submit').on('click', function(event) {
     event.preventDefault()
     $target = $(event.currentTarget)
-    stripe.createToken(card).then(function(result) {
-        if (result.error) {
-            var errorElement = document.getElementById('card-errors')
-            errorElement.textContent = result.error.message
-        } else {
-            const enable = $target.data('enable')
-            if (typeof enable === 'undefined') {
-                stripeTokenHandler(result.token)
+
+    if ($('#agree').is(':checked')) {
+        stripe.createToken(card).then(function(result) {
+            if (result.error) {
+                var errorElement = document.getElementById('card-errors')
+                errorElement.textContent = result.error.message
+            } else {
+                const enable = $target.data('enable')
+                if (typeof enable === 'undefined') {
+                    stripeTokenHandler(result.token)
+                }
+                $target.attr('data-enable', 0)
             }
-            $target.attr('data-enable', 0)
-        }
-    })
+        })
+    }
 })
