8a09886f8c0b8626605a62b6cd8d44a5b864cc97
pandog <yunhaihuang@gmail.com>
2018-08-24 22:54:45 +0700

    .

---
+++ resources/views/backend/component/form/vimeo.blade.php
@@ -0,0 +1,17 @@
+<tr>
+    <td class="aside">{{ $field_label }}</td>
+    <td>
+        <input id='{{ $field_id ?? $field_name }}' name="{{ $field_name }}" class="form-control" value="{{ $field_value }}"
+            @if (!empty($field_attribute))
+            @foreach ($field_attribute as $key => $value)
+                {{ $key }}="{{ $value }}"
+            @endforeach
+            data-name="{{ $field_name }}"
+            @endif
+        ></input>
+        
+        @if (!empty($errors->first($field_name)))
+            <span class="text-danger d-block mt-2">{{ $errors->first($field_name) }}</span>
+        @endif
+    </td>
+</tr>
