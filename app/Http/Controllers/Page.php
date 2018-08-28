0d5209efdca5048e0214dfa8e7966b6bfed58b56
pandog <yunhaihuang@gmail.com>
2018-08-28 22:50:14 +0700

    .

--- app/Http/Controllers/Page.php
+++ app/Http/Controllers/Page.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Service\UtmTracking\UtmTrackingService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class Page extends Base
 {
@@ -56,6 +57,69 @@ class Page extends Base
             }
         }
 
+        dd('done');
+    }
+    
+    public function getCleanUpPoster()
+    {
+        $delete_flag = request()->get('delete_flag');
+        
+        $q = 'select lesson_details.id, lesson_details.poster, media.id, media.path 
+        FROM lesson_details, media 
+        WHERE lesson_details.poster = media.id and lesson_details.deleted_at is null';
+        $list = \DB::select($q);
+        foreach ($list as $item) {
+            print_r('<pre>');
+            print_r(Storage::disk('media')->path($item->path));
+            print_r('</pre>');
+            if ($delete_flag) {
+                Storage::disk('media')->delete($item->path);
+            }
+        }
+
+        dd('done');
+    }
+    
+    public function getResizePoster()
+    {
+        $resize = request()->get('resize');
+        
+        $q = 'select lesson_details.id, lesson_details.poster, media.* 
+        FROM lesson_details, media 
+        WHERE lesson_details.poster = media.id and lesson_details.deleted_at is null';
+        $list = \DB::select($q);
+        
+        $resized = [];
+        foreach ($list as $item) {
+            $location = $item->location;
+            $extension = substr($item->hash_name, strrpos($item->hash_name, '.'));
+            $bk_name = substr($item->hash_name, 0, strrpos($item->hash_name, '.')) . '_original' . $extension;
+        
+            $old = Storage::disk('media')->path("{$item->path}");
+            $new = Storage::disk('media')->path("{$location}/{$bk_name}");
+            
+            if (in_array($old, $resized)) {
+                continue;
+            }
+            array_push($resized, $old);
+            // ngang 210
+            // cao 120
+            print_r('<pre>');
+            print_r($old);
+            print_r('</pre>');
+            if ($resize && file_exists($old)) {
+                print_r('<pre>move:');
+                print_r($old);
+                print_r('</pre>');
+                copy($old, $new);
+                Image::make($old)
+                    ->resize(210, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    })
+                    ->save($old);
+            }
+        }
         dd('done');
     }
 }
