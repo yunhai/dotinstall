615180326d6189b0a3c6c973877963a060dc1da0
pandog <yunhaihuang@gmail.com>
2018-09-10 23:39:07 +0700

    .

--- resources/views/component/top/panel.blade.php
+++ resources/views/component/top/panel.blade.php
@@ -20,7 +20,7 @@
                         @if ($item['type'] == YOUTUBE_TYPE_VIDEO)
                             <iframe class='youtube-video' width="100%" height="211" src="https://www.youtube.com/embed/{{ $item['youtube_id'] }}?enablejsapi=1&version=3&playerapiid=ytplayer" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                         @elseif ($item['type'] == YOUTUBE_TYPE_VIMEO)
-                            <iframe class='vimeo-video' src="https://player.vimeo.com/video/{{ $item['youtube_id'] }}?autoplay=1&loop=1&autopause=0&api=1" width="100%" height="211" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                            <iframe class='vimeo-video' src="https://player.vimeo.com/video/{{ $item['youtube_id'] }}?autoplay=1&loop=1&autopause=0&api=1&playsinline=0" width="100%" height="211" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                         @elseif ($item['type'] == YOUTUBE_TYPE_IMAGE)
                             @php $path = $item['path'] ?? ''; @endphp
                             <a href='{{ $item['url'] }}'>
