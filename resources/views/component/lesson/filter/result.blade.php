6cfa1f693c08c8b6ee71191120bd72b78d7a75bd
pandog <yunhaihuang@gmail.com>
2018-09-14 20:55:07 +0700

    lesson listing

--- resources/views/component/lesson/filter/result.blade.php
+++ resources/views/component/lesson/filter/result.blade.php
@@ -28,20 +28,16 @@
             <div>
                 <ul class="list-group w-100">
                     @foreach ($lesson_item as $lesson)
-                        <li class="list-group-item list-group-item-lesson px-0" style='position: relative; height: 100%;
-                        
-                        '>
+                        <li class="list-group-item list-group-item-lesson px-0">
                             @php
                                 $first_lesson_detail = $lesson['lesson_details'][0] ?? ['free_mode' => 0, 'new_mode' => 0];
                                 if ($first_lesson_detail) {
                                     $poster = $first_lesson_detail['posters'][0]['path'] ?? '';
-                                    $caption = $first_lesson_detail['caption'] ?? '';
                                 }
                             @endphp
                             <div class="@pc col-9 float-left @endpc @sp col-12 @endsp px-0">
-                            @php $width=43; @endphp
-                                <div>
-                                    <div style="float: left; margin-right: 10px;">
+                                <div class='lession--item'>
+                                    <div class="lession--item__image">
                                         @if ($first_lesson_detail['free_mode'] == constant('LESSON_DETAIL_FREE_MODE_FREE') ||
                                             $first_lesson_detail['new_mode'] == constant('LESSON_DETAIL_NEW_MODE_NEW')
                                         )
@@ -60,23 +56,25 @@
                                         <a href="{{ route('lesson.detail', ['lesson_id' => $lesson['id']] ) }}" >
                                             <img src='@media_path($poster)' width='@pc 140 @endpc @sp 120 @endsp' />
                                         </a>
+                                        <span class='lesson--item__learning_count'>
+                                            {{ number_format($lesson['lesson_learning_count']) }} 人が学習中
+                                        </span>
                                     </div>
-                                    <div style='position: absolute;top: {{ $width }}%; left: 150px;'>
+                                    <div class='lesson--item__content'>
                                         <a href="{{ route('lesson.detail', ['lesson_id' => $lesson['id']]) }}" >
                                             @php $is_all_finish = ($lesson['lesson_detail_close_count'] >= $lesson['video_count']) ; @endphp
                                             <span @if($is_all_finish) style='text-decoration: line-through' @endif>{{ $lesson['name'] }}（全{{ $lesson['video_count'] }}回）</span>
                                         </a>
                                     </div>
-                                    <div class="clearfix"></div>
                                 </div>
-                                <span style='margin-top: 3px; display: block;width:140px; text-align:center;'>{{ number_format($lesson['lesson_learning_count']) }} 人が学習中</span>
+                                <div class="clearfix"></div>
                             </div>
-                            <div class="lesson__status @pc col-3 text-right float-right d-flex align-items-center justify-content-end @endpc @sp mar_t5 @endsp px-0">
+                            <div class="lesson--item__my_styding @pc col-3 text-right float-right d-flex align-items-center justify-content-end @endpc @sp col-12 @endsp px-0">
                                 @if (!empty(Auth::check()))
                                     @if ($is_all_finish)
                                         <span class="btn-all-complete">全て完了</span>
                                     @else
-                                        <span style="min-width: 38px;">完了 / {{ $lesson['lesson_detail_close_count'] }}</span>
+                                        完了 / {{ $lesson['lesson_detail_close_count'] }}
                                     @endif
                                 @endif
                             </div>
