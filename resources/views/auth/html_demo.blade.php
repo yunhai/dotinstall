300f7aa10cd419b4134eb6003bc1821a54442630
Lê Thúy Oanh <lethuyoanhbt@gmail.com>
2018-09-15 12:44:50 +0700

    fix css

--- resources/views/auth/html_demo.blade.php
+++ resources/views/auth/html_demo.blade.php
@@ -65,11 +65,11 @@
                                         <div class="card-header">【月額会員】</div>
                                         <div class="card-body">
                                             <ul class="card-info">
-                                                <li>・全ての動画が見放題となります。</li>
-                                                <li>・月額制:\980円（税込み）</li>
-                                                <li>・クレジットカード決済のみとなります。</li>
-                                                <li>・毎月自動決済されます。</li>
-                                                <li>・購入後、設定変更画面から自動更新を停止することもできます。</li>
+                                                <li>・ 全ての動画が見放題となります。</li>
+                                                <li>・ 月額制:\980円（税込み）</li>
+                                                <li>・ クレジットカード決済のみとなります。</li>
+                                                <li>・ 毎月自動決済されます。</li>
+                                                <li>・ 購入後、設定変更画面から自動更新を停止することもできます。</li>
                                             </ul>
                                             <hr>
                                             <form method="POST" aria-label="新規ユーザー登録">
@@ -184,10 +184,10 @@
                                     <p>メールアドレスは運営側からの連絡をする際に必要となります。</p>
                                     <p class="mar_b0">パスワードを設定したい場合は、ユーザー登録後に設定変更画面からおこなってください。</p>
                                 @else
-                                    <a href="/auth/line" class="btn btn-line btn-block">LINEログイン</a>
+                                   <!--  <a href="/auth/line" class="btn btn-line btn-block">LINEログイン</a>
                                     <a href="/auth/twitter" class="btn btn-twitter btn-block">
                                         <span><img class="img-fluid" src="/img/twitter.png"></span> <span class="btn-twitter-label">Twitterログイン</span>
-                                    </a>
+                                    </a> -->
                                     <a href="/auth/facebook" class="btn btn-facebook btn-block">
                                         <span><img class="img-fluid" src="/img/facebook.png"></span> <span class="btn-facebook-label">Facebookログイン</span>
                                     </a>
