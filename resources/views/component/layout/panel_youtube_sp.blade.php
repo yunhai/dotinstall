e0ae3b4b215c85609c093e530ce0c27bd3829fbc
pandog <yunhaihuang@gmail.com>
2018-09-20 21:24:16 +0700

    top: hide 新規登録はこちら！ after login

--- resources/views/component/layout/panel_youtube_sp.blade.php
+++ resources/views/component/layout/panel_youtube_sp.blade.php
@@ -7,7 +7,9 @@
                     <div class="card-sign p-0" id="pc-panel-left">
                         <!--<p class="card-text card-text-sign">無料レッスンもいっぱいご用意！</p>-->
                         <p class="card-text card-text-sign last">{!! nl2br($global_setting['page_caption']) !!}</p>
+                        @if (!Auth::check())
                         <a href="{{ route('register') }}" class="card-sign-button" style='margin-right: 15px;'>新規登録はこちら！</a>
+                        @endif
                     </div>
                 </div>
             </div>
