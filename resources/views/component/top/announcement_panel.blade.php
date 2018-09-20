b0da0f0bacd4c8d31f01e90480a5312ffef0d065
Lê Thúy Oanh <lethuyoanhbt@gmail.com>
2018-09-20 23:04:00 +0700

    fix css announcement

--- resources/views/component/top/announcement_panel.blade.php
+++ resources/views/component/top/announcement_panel.blade.php
@@ -1,29 +1,27 @@
 <div id="notice-content">
     <div class="box mb-0">
-        <div class="notice-content">
-            <div class="notice-content--announcement">
-                <div class="heading">
-                    <span>【お知らせ】</span>
-                </div>
-                <div class="content--announcement">
-                @if ($announcement)
-                @foreach ($announcement as $item)
-                    <div class='announcement--date'>{{ $item['post_date'] }}&nbsp;</div>
-                    <div class="announcement--content" data-toggle="tooltip" data-placement="bottom" title="{{ $item['content'] }}">
-                        {{ $item['content'] }}
-                    </div>
-                @endforeach
-                @endif
-                </div>
+        <div class="notice-content--announcement">
+            <div class="heading">
+                <span>[お知らせ]</span>
             </div>
-            <div class="notice-content--ad">
-                @if ($ad)
-                <a href="{{ $ad['link'] }}" title="{{ $ad['name'] }}">
-                    @php $path = $ad['media']['path']; @endphp
-                    <img src="@media_path($path)" alt="{{ $ad['name'] }}" class="notice-content--ad__img" />
-                </a>
-                @endif
+            <div class="content--announcement">
+            @if ($announcement)
+            @foreach ($announcement as $item)
+                <div class='announcement--date'>{{ $item['post_date'] }}&nbsp;</div>
+                <div class="announcement--content" data-toggle="tooltip" data-placement="bottom" title="{{ $item['content'] }}">
+                    {{ $item['content'] }}
+                </div>
+            @endforeach
+            @endif
             </div>
+        </div>
+        <div class="notice-content--ad">
+            @if ($ad)
+            <a href="{{ $ad['link'] }}" title="{{ $ad['name'] }}">
+                @php $path = $ad['media']['path']; @endphp
+                <img src="@media_path($path)" alt="{{ $ad['name'] }}" class="notice-content--ad__img" />
+            </a>
+            @endif
         </div>
     </div>
 </div>
