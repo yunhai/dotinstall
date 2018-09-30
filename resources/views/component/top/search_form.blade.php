36546b0f8e16443841b558febd0d5f43cfc0e113
pandog <yunhaihuang@gmail.com>
2018-09-30 20:23:31 +0700

    search form

--- resources/views/component/top/search_form.blade.php
+++ resources/views/component/top/search_form.blade.php
@@ -1,29 +1,7 @@
 <form action="{{ route('top.search') }}" class="@pc offset-md-2 form-inline @endpc @sp col-sm-12 @endsp top-search">
-  @pc
     @php
-        $diff = array_get($filter_form, 'input_value.difficulty');
-        $cate = array_get($filter_form, 'input_value.category');
         $keyword = array_get($filter_form, 'input_value.keyword');
     @endphp
-    <div class="form-group">
-        <label for="filter-difficulty">段階</label>
-        <select id='filter-difficulty' name="difficulty" class="form-control mx-sm-3 j-lessonFilter top-search--select">
-            <option value="" @if (empty($diff)) selected @endif>全ての動画</option>
-            @foreach ($filter_form['difficulty'] as $difficulty_id => $difficulty)
-                <option value="{{ $difficulty_id }}" @if ($diff == $difficulty_id) selected @endif>{{ $difficulty }}</option>
-            @endforeach
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="filter-category">カテゴリ</label>
-        <select id='filter-category' name="category" class="form-control mx-sm-3 j-lessonFilter top-search--select">
-            <option value="" @if (empty($cate)) selected @endif>全ての動画</option>
-            @foreach ($filter_form['category'] as $cat_id => $cat)
-                <option value="{{ $cat_id }}" @if ($cate == $cat_id) selected @endif>{{ $cat }}</option>
-            @endforeach
-        </select>
-    </div>
-    @endpc
     <div class="form-group">
         <div id="pg-search-input">
             <div class="input-group col-md-12">
